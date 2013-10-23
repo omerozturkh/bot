591f252c79b5a2d4bd7247b8395b4c4b4d64a696
omerozturkh <omerozturkh@gmail.com>
2013-10-23 13:08:26 +0300

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'SlIAsprLCks8zwo5b3LS3Q');
-define('CONSUMER_SECRET', '325wsusKGBSCNiIj2aKrkJaF8Mdl1bT8sJbiyByws');
-define('OAUTH_CALLBACK', 'http://happydigital.com.tr/bot/callback.php');
+define('CONSUMER_KEY', 'uygulamanızın consumer keyi');
+define('CONSUMER_SECRET', 'uygulamanızın consumer secreti');
+define('OAUTH_CALLBACK', 'http://localhost/bot/callback.php');
